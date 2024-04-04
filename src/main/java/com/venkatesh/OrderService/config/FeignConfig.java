97639cce6a55334e5422edaafaf6f6b0b612a366
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-04 12:54:58 -0400

    Implementing ErrorDecoder to handle Exceptions

---
+++ src/main/java/com/venkatesh/OrderService/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.venkatesh.OrderService.config;
+
+import com.venkatesh.OrderService.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    ErrorDecoder errorDecoder(){
+        return new CustomErrorDecoder();
+    }
+}
---
+++ src/main/java/com/venkatesh/OrderService/external/response/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.venkatesh.OrderService.external.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String errorCode;
+    private String errorMessage;
+}
