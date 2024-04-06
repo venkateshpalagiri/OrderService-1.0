6d846c601fffb780895f5fcf5cc2625085948e77
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-05 21:36:05 -0400

    Revert "splunk log monitering tool integration"
    This reverts commit 6db1b1f41f6de1b1dfac25f38af4bd8dca2adc7c.

--- src/main/java/com/venkatesh/OrderService/controller/OrderController.java
+++ src/main/java/com/venkatesh/OrderService/controller/OrderController.java
@@ -3,8 +3,6 @@ package com.venkatesh.OrderService.controller;
 import com.venkatesh.OrderService.model.OrderRequest;
 import com.venkatesh.OrderService.service.OrderService;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +18,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    Logger logger= LogManager.getLogger(OrderController.class);
-
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest){
-        logger.info("placeOrder method calling..");
         long orderId=orderService.placeOrder(orderRequest);
         return new ResponseEntity<>(orderId, HttpStatus.OK);
 
--- src/main/java/com/venkatesh/OrderService/service/OrderServiceImpl.java
+++ src/main/java/com/venkatesh/OrderService/service/OrderServiceImpl.java
@@ -4,24 +4,20 @@ import com.venkatesh.OrderService.entity.Order;
 import com.venkatesh.OrderService.external.client.ProductService;
 import com.venkatesh.OrderService.model.OrderRequest;
 import com.venkatesh.OrderService.repository.OrderRepository;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
 @Service
-
+@Log4j2
 public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
     private ProductService productService;
-
-    Logger logger= LogManager.getLogger(OrderServiceImpl.class);
     @Override
     public long placeOrder(OrderRequest orderRequest){
 
@@ -30,11 +26,10 @@ public class OrderServiceImpl implements OrderService{
         /*Call the Payment Service to complete the payment as well,
         if payment successfull mark order as COMPLETE, Else CANCELLED
          */
-        
-        logger.info("Placing Order Request: {}", orderRequest);
+        log.info("Placing Order Request: {}", orderRequest);
 
         productService.reduceQuantity(orderRequest.getProductId(),orderRequest.getQuantity());
-        logger.info("Creating Order with status CREATED");
+        log.info("Creating Order with status CREATED");
 
 
         Order order=Order.builder()
@@ -45,7 +40,7 @@ public class OrderServiceImpl implements OrderService{
                 .quantity(orderRequest.getQuantity())
                 .build();
         order=orderRepository.save(order);
-        logger.info("Order Places Successfully with Order Id:{}",order.getId());
+        log.info("Order Places Successfully with Order Id:{}",order.getId());
         return order.getId();
 
     }
