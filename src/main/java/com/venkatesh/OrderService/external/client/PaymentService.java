29e3bf4be89ec51a75413ded7bc3f0f6ff1e25a5
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-11 11:47:09 -0400

    ReateLimiter-Resilience4j

--- src/main/java/com/venkatesh/OrderService/external/client/PaymentService.java
+++ src/main/java/com/venkatesh/OrderService/external/client/PaymentService.java
@@ -1,14 +1,20 @@
 package com.venkatesh.OrderService.external.client;
 
+import com.venkatesh.OrderService.exception.CustomException;
 import com.venkatesh.OrderService.model.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e){
+        throw new CustomException("Payment Service is down!","PAYMENT_SERVICE_UNAVAILABLE",500);
+    }
 }
--- src/main/java/com/venkatesh/OrderService/external/client/ProductService.java
+++ src/main/java/com/venkatesh/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.venkatesh.OrderService.external.client;
 
+import com.venkatesh.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -13,4 +16,9 @@ public interface ProductService {
 //    No need to mention public as by default methods are public
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product Service is down!","PRODUCT_SERVICE_UNAVAILABLE",500);
+    }
+
 }
