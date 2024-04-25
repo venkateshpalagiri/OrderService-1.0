5eb9ccd43eb15e76f622ccb4a125fe212319edb5
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-25 11:22:45 -0400

    added @PreAuthorize over methods in controller

--- src/main/java/com/venkatesh/OrderService/controller/OrderController.java
+++ src/main/java/com/venkatesh/OrderService/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,13 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
     @Autowired
     private OrderService orderService;
-
+    @PreAuthorize("hasAuthority('Customer')")
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest){
         long orderId=orderService.placeOrder(orderRequest);
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId){
         OrderResponse orderResponse=orderService.getOrderDetails(orderId);
