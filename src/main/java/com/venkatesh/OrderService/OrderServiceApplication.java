1434e9d134c1e93bc2bd23434e0db0c833b572ed
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-25 09:11:59 -0400

    Adding Interceptors

--- src/main/java/com/venkatesh/OrderService/OrderServiceApplication.java
+++ src/main/java/com/venkatesh/OrderService/OrderServiceApplication.java
@@ -1,15 +1,29 @@
 package com.venkatesh.OrderService;
 
+import com.venkatesh.OrderService.external.intercept.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableFeignClients
 public class OrderServiceApplication {
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+	@Autowired
+	private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
@@ -17,7 +31,36 @@ public class OrderServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
+		RestTemplate restTemplate
+				=new RestTemplate();
+		restTemplate.setInterceptors(
+				Arrays.asList(
+						new RestTemplateInterceptor(
+								auth2AuthorizedClientManager(clientRegistrationRepository,
+										oAuth2AuthorizedClientRepository)
+						)
+				)
+		);
 		return new RestTemplate();
+	}
+	@Bean
+	public OAuth2AuthorizedClientManager auth2AuthorizedClientManager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+	){
+		OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider
+				= OAuth2AuthorizedClientProviderBuilder
+				.builder()
+				.clientCredentials()
+				.build();
+
+		DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager
+				= new DefaultOAuth2AuthorizedClientManager(
+						clientRegistrationRepository,oAuth2AuthorizedClientRepository);
+		oAuth2AuthorizedClientManager.setAuthorizedClientProvider(
+				oAuth2AuthorizedClientProvider
+		);
+		return oAuth2AuthorizedClientManager;
 	}
 
 }
